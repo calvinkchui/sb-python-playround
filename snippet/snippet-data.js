ae01c734ed469c9b54485c145f96bd989b65809d
calvinkchui <calvin.kchui@gmail.com>
2023-12-05 16:23:37 +0800

    Update snippet-data.js
    update snip

--- snippet/snippet-data.js
+++ snippet/snippet-data.js
@@ -1,5 +1,10 @@
 var snippets = [
   {
+    title: "Varible",  
+    syntax: "python",  
+    codeUrl: "basic/var.py",
+  },
+  {
     title: "function",  
     syntax: "python",  
     codeUrl: "basic/function.py",

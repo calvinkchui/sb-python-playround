c2af8861bafc98f46279d4f20d47a9452f43e8e2
calvin <calvin.kchui@gmail.com>
2024-01-24 01:27:31 +0000

    price

--- tmp/parsePrice.py
+++ tmp/parsePrice.py
@@ -36,17 +36,30 @@ def getFirstMerchantPrice(url):
     # tag.select('.text-price-number'): [<span class="text-price-number" data-price="9950.0">9,950</span>, <span class="text-price-number" data-price="9550.0">9,550</span>]
     rst['name'] = tag.find("p", attrs={"class": "quotation-merchant-name"} ).find("a").text
     
-    
+    #quote-source
+    '''
+    <div class="quote-source">
+      <div>2024-01-23</div>
+      <div>更新</div>
+      <img src="/images/icon/stock_dot.svg" class="stock-dot">
+      </div>
+      <div class="quote-stock-status-LST">少量存貨</div>                                                                                                                                                                        </div>    
+    '''
+    rst['quote'] = tag.find("div", attrs={"class": "quote-source"} ).text.replace('\n',' ')
     rst['price'] = float(tag.select('.text-price-number')[0]['data-price'])
 
-    
     
     return rst;
 
 def testRun():
+    baseURL = "https://www.price.com.hk/product.php?p="
+ 
     rst = getFirstMerchantPrice(url = "https://www.price.com.hk/product.php?p=594865") # Fold 5 512
     print(rst)  
     rst = getFirstMerchantPrice(url = "https://www.price.com.hk/product.php?p=594860") # Fold 5 256
     print(rst)  
+    rst = getFirstMerchantPrice(url = baseURL + "574102") # s23 U 256
+    print(rst)      
+
 
 testRun()

a838a6c1fbbcc3b61897c9faa718c33d4ab724af
calvin <calvin.kchui@gmail.com>
2024-07-05 10:14:12 +0000

    fastapi

--- topic/web/fastapi_api.py
+++ topic/web/fastapi_api.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
 
 #app = FastAPI()
-app = FastAPI(root_path="/proxy/8000/") 
+app = FastAPI(root_path="/proxy/8000/",
+
+# document
+    title="Sandbox FastAPI",
+    description="Fast API - demo application",
+    summary="test functions"
+
+) 
+
 
 '''
 https://fastapi.tiangolo.com/tutorial/first-steps/
 
+> cd topic/web/
 > python3 -m uvicorn fastapi_api:app --reload
 '''
 
+'''
+Behind the Proxy
+* https://fastapi.tiangolo.com/advanced/behind-a-proxy/
+
+app = FastAPI(root_path="/proxy/8000/") 
+'''
+# First Step
 @app.get("/")
 async def root():
     return {"message" : "Hello World!"}
@@ -31,9 +47,21 @@ Generated openapi - http://xxx/openapi.json
 '''
 
 
-'''
-Behind the Proxy
-* https://fastapi.tiangolo.com/advanced/behind-a-proxy/
 
-app = FastAPI(root_path="/proxy/8000/") 
-'''
+
+# Path Parameter
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+
+# Query Parameter
+# path: /items/?skip=0&limit=10
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+

cdac0f4a2f06edf13e9b13a7a0ff2c985ac29d0a
calvin <calvin.dev@gmail.com>
2024-09-13 14:15:12 +0800

    update

--- snippet/snippet-data.js
+++ snippet/snippet-data.js
@@ -122,13 +122,12 @@ with Demo() as x:
   title: "Naming",
   content: `
 
-\`\`\`
-module_name, package_name, 
-ClassName, method_name, ExceptionName, function_name, 
-GLOBAL_CONSTANT_NAME, global_var_name, 
-instance_var_name, function_parameter_name, 
-local_var_name, query_proper_noun_for_thing, send_acronym_via_https
-\`\`\`
+\`module_name\`, \`package_name\`, 
+\`ClassName\`, \`method_name\`,\`ExceptionName\`,\`function_name\`, 
+\`GLOBAL_CONSTANT_NAME\`,\`global_var_name\`, 
+\`instance_var_name\`,\`function_parameter_name\`, 
+\`local_var_name\`,\`query_proper_noun_for_thing\`,\`send_acronym_via_https\`
+
 
 * [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html#316-naming)
 `
